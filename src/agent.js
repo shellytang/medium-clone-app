64c056b93744b2e7aa3f89b10a97824f06fbd2f8
Shelly Tang <shellytang@gmail.com>
2018-02-06 18:25:29 -0800

    updated to include put request

--- src/agent.js
+++ src/agent.js
@@ -22,7 +22,8 @@ const Articles = {
 const Auth = {
   current: () => request.get('/user'),
   login: (email, password) => request.post('/users/login', { user: { email, password }}),
-  register: (username, email, password) => request.post('/users', { user: { username, email, password }})
+  register: (username, email, password) => request.post('/users', { user: { username, email, password }}),
+  save: user => request.put('/user', { user })
 }
 
 export default {
